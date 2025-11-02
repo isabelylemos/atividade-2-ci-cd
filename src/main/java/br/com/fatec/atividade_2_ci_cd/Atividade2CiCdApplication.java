8bed5e209edc01b2cca675c844cfbae28d4e7022
Isabely Lemos <isabelylemos24@gmail.com>
2025-11-02 16:06:06 -0300

    Initial commit

---
+++ src/main/java/br/com/fatec/atividade_2_ci_cd/Atividade2CiCdApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.atividade_2_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade2CiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade2CiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/atividade_2_ci_cd/Atividade2CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.atividade_2_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade2CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
